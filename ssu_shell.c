9efe25ed2206f18e023e9b601a03e755a8f4324a
ChanYangYu <clsrnthd7@gmail.com>
2020-09-04 09:05:29 -0700

    Not use pipe() version

--- ssu_shell.c
+++ ssu_shell.c
@@ -6,11 +6,13 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <signal.h>
 #define MAX_INPUT_SIZE 1024
 #define MAX_TOKEN_SIZE 64
 #define MAX_NUM_TOKENS 64
-
-int backup;
+int err_backup;
+int out_backup;
+int in_backup;
 
 /* Splits the string by space and returns the array of tokens
 *
@@ -40,14 +42,25 @@ char **tokenize(char *line)
  	return tokens;
 }
 
+void signal_handler(int signo)
+{
+	remove(".error.txt");
+	printf("\n");
+	exit(0);
+}
+
 int main(int argc, char* argv[]) {
 	struct stat statbuf;
 	char  line[MAX_INPUT_SIZE];            
 	char *args[MAX_TOKEN_SIZE];
 	char  **tokens;              
-	int i, status, fd;
+	int i, status, fd, pipe_point;
+	int is_pipe_after;
+	int pipe_out, pipe_in;
 	FILE* fp;
 
+	//시그널처리
+	signal(SIGINT, signal_handler);
 	//모드 확인
 	if(argc == 2) {
 		fp = fopen(argv[1],"r");
@@ -80,18 +93,88 @@ int main(int argc, char* argv[]) {
 			continue;
 		//line 토큰화
 		tokens = tokenize(line);
-		//args배열 채움
-		for(i=0;tokens[i]!=NULL;i++)
-			args[i] = tokens[i];
-		args[i] = NULL;
 		//에러 체크 임시파일 생성
 		if((fd = open(".error.txt",O_RDWR|O_CREAT|O_TRUNC, 0644)) < 0){
 			fprintf(stderr,"file create error\n");
 			exit(1);
 		}
 		//redirection STDERR
-		backup = dup(2);
-		dup2(fd, 2);
+		err_backup = dup(STDERR_FILENO);
+		dup2(fd, STDERR_FILENO);
+		pipe_point = 0;
+		is_pipe_after = 0;
+		//args배열 채움
+		for(i=0;tokens[i] != NULL;i++){
+			if(!strcmp(tokens[i], "|")){
+				//args cut
+				args[pipe_point] = NULL;
+				// |문자가 나오면 STDOUT을  Redirection
+				if(!strcmp(tokens[i], "|")){
+					//이전 pipe파일이 있으면( | 명령어 | 인 경우)
+					if(access("pipe.txt", F_OK) == 0 )
+						pipe_out = open("pipe2.txt",O_WRONLY|O_CREAT|O_TRUNC, 0644);
+					else
+						//pipe파일 생성
+						pipe_out = open("pipe.txt",O_WRONLY|O_CREAT|O_TRUNC, 0644);
+					out_backup = dup(STDOUT_FILENO);
+					dup2(pipe_out, STDOUT_FILENO);
+				}
+				// |문자가 이후이면 STDIN을 Redirection 
+				if(is_pipe_after){
+					//파이프는 반드시 아래에서 rename되므로 pipe.txt로 연다.
+					pipe_in = open("pipe.txt",O_RDONLY);
+					in_backup = dup(STDIN_FILENO);
+					dup2(pipe_in, STDIN_FILENO);
+				}
+				//자식프로세스 생성
+				if(fork() == 0){
+					if(execvp(args[0],args) < 0){
+						fprintf(stderr,"exec error\n");
+						exit(1);
+					}
+				}
+				//자식프로세스 대기
+				if(wait(&status) < 0){
+					fprintf(stderr,"wait error\n");
+					exit(1);
+				}
+				//pipe_point 0으로 초기화
+				pipe_point = 0;
+
+				//파이프 사이에 낀 경우도 있으므로 is_pipe_after순서변경
+				//STDIN 롤백
+				if(is_pipe_after){
+					dup2(in_backup, STDIN_FILENO);
+					close(in_backup);
+					//pipe처리 완료
+					is_pipe_after = 0;
+					//파일디스크립터 close
+					close(pipe_in);
+					//이미 다읽었으므로 파일삭제
+					remove("pipe.txt");
+				}
+				//STDOUT 롤백
+				if(!strcmp(tokens[i], "|")){
+					dup2(out_backup, STDOUT_FILENO);
+					close(out_backup);
+					//pipe처리 필요
+					is_pipe_after = 1;
+					close(pipe_out);
+					//이전 pipe는 사용됐으므로 pipe rename
+					if(access("pipe2.txt", F_OK) == 0)
+						rename("pipe2.txt", "pipe.txt");
+				}
+			}
+			else
+				args[pipe_point++] = tokens[i];
+		}
+		// |문자가 이후이면 STDIN을 Redirection 
+		if(is_pipe_after){
+			//파이프는 반드시 아래에서 rename되므로 pipe.txt로 연다.
+			pipe_in = open("pipe.txt",O_RDONLY);
+			in_backup = dup(STDIN_FILENO);
+			dup2(pipe_in, STDIN_FILENO);
+		}
 		//자식프로세스 생성
 		if(fork() == 0){
 			if(execvp(args[0],args) < 0){
@@ -104,9 +187,20 @@ int main(int argc, char* argv[]) {
 			fprintf(stderr,"wait error\n");
 			exit(1);
 		}
-		//roll back
-		dup2(backup, STDERR_FILENO);
-		close(backup);
+		//STDIN 롤백
+		if(is_pipe_after){
+			dup2(in_backup, STDIN_FILENO);
+			close(in_backup);
+			//pipe처리 완료
+			is_pipe_after = 0;
+			//파일디스크립터 close
+			close(pipe_in);
+			//이미 다읽었으므로 파일삭제
+			remove("pipe.txt");
+		}
+		//pipe_point 0으로 초기화
+		dup2(err_backup, STDERR_FILENO);
+		close(err_backup);
 		close(fd);
 		//에러파일 체크
 		if(stat(".error.txt",&statbuf) < 0){
