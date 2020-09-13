8688b572213a185dca5f05793ec7474f0862495e
ChanYangYu <clsrnthd7@gmail.com>
2020-09-13 22:52:51 +0900

    pps 구현완료

--- pps.c
+++ pps.c
@@ -3,8 +3,10 @@
 #include <time.h>
 #include <pwd.h>
 #include <sys/param.h>
+#include <curses.h>
 
 int is_aOption, is_uOption, is_xOption;
+int MAX_WIDTH;
 long memtotal;
 time_t start_time,cpu_time;
 double cpu_usage;
@@ -12,6 +14,7 @@ char tokens[31][MAX_TOKEN_SIZE];
 
 int isNum(char* name);
 void print_no_option();
+void print_one_option();
 void print_uOption();
 void get_info(unsigned long long totaltime, unsigned long long starttime);
 void get_memtotal();
@@ -20,12 +23,17 @@ int main(int argc, char* argv[])
 {
 	FILE* fp;
 	DIR* dirp;
+	int user, process_uid;
 	struct dirent* dp;
 	struct stat statbuf;
 	struct passwd *pw;
 	int i, j, c;
-	long nice, num_thread;
+	int y = 0, x = 0;
 
+	initscr();
+	getmaxyx(stdscr, y, x);
+	endwin();
+	MAX_WIDTH = x;
 	if(argc > 1){
 		for(i = 0; argv[1][i] !='\0'; i++)
 			switch(argv[1][i]){
@@ -36,13 +44,16 @@ int main(int argc, char* argv[])
 			}
 	}
 	if(is_uOption){
-		printf("USER         PID %%CPU %%MEM    VSZ   RSS TTY      STAT START   TIME COMMAND\n");
+		printf("USER       PID %%CPU %%MEM    VSZ   RSS TTY      STAT START   TIME COMMAND\n");
+		//창이 너무작으면
+		if(MAX_WIDTH <= 67)
+			MAX_WIDTH *= 2;
 		get_memtotal();
 	}
 	else if(is_aOption || is_xOption)
-		printf("    PID TTY      STAT   TIME COMMAND\n");
+		printf("  PID TTY      STAT   TIME COMMAND\n");
 	else{
-		printf("    PID TTY          TIME CMD\n");
+		printf("  PID TTY          TIME CMD\n");
 		print_no_option();
 		exit(0);
 	}
@@ -50,6 +61,7 @@ int main(int argc, char* argv[])
 		fprintf(stderr,"/proc open fail\n");
 		exit(1);
 	}
+	user = getuid();	
 	chdir("/proc");
 	while((dp = readdir(dirp)) != NULL){
 		memset(tokens, 0, sizeof(tokens));
@@ -59,7 +71,8 @@ int main(int argc, char* argv[])
 				exit(1);
 			}
 			//소유자 아이디 문자열화
-			pw = getpwuid(statbuf.st_uid);
+			process_uid = statbuf.st_uid;
+			pw = getpwuid(process_uid);
 			if(strlen(pw->pw_name) > 8){
 				strncpy(tokens[0],pw->pw_name, 7);
 				strcat(tokens[0],"+");
@@ -83,8 +96,35 @@ int main(int argc, char* argv[])
 				}
 				tokens[i][j] = '\0';
 			}
+			//a옵션 단독일경우
+			if(is_aOption && !is_xOption){
+				if(!strcmp(tokens[7], "0")){
+					fclose(fp);
+					chdir("..");
+					continue;
+				}
+			}
+			//x옵션 단독일 경우
+			else if(is_xOption && !is_aOption){
+				if(user != process_uid){
+					fclose(fp);
+					chdir("..");
+					continue;
+				}
+			}
+			//u옵션 단독
+			else if(is_uOption && !is_aOption && !is_xOption){
+				if(user != process_uid || !strcmp(tokens[7], "0")){
+					fclose(fp);
+					chdir("..");
+					continue;
+				}
+			}
+			//print 옵션확인
 			if(is_uOption)
 				print_uOption();
+			else if(is_aOption || is_xOption)
+				print_one_option();
 			fclose(fp);
 			chdir("..");
 		}
@@ -140,15 +180,43 @@ void get_info(unsigned long long totaltime, unsigned long long starttime)
 	fclose(fp);
 }
 
+void get_memtotal()
+{
+	FILE* fp;
+	char token[MAX_TOKEN_SIZE];
+	int c, i;
+	if((fp = fopen("/proc/meminfo", "r")) == NULL){
+		fprintf(stderr,"meminfo open error\n");
+		exit(1);
+	}
+	while((c = fgetc(fp)) != '\n'){
+		if(c >= '0' && c <= '9'){
+			i = 0;
+			token[i++] = c;
+			c = fgetc(fp);
+			while(c >= '0' && c <= '9'){
+				token[i++] = c;
+				c = fgetc(fp);
+			}
+			token[i] = '\0';
+			break;
+		}
+		
+	}
+	memtotal = atol(token);
+}
+
 void print_uOption()
 {
 	long nice, num_thread, rss, vsz;
 	struct tm *tmbuf;
 	time_t t;
 	unsigned long long starttime,totaltime;
-	char path[MAX_INPUT_SIZE], *ptr, buffer[MAX_TOKEN_SIZE];
+	char path[MAX_INPUT_SIZE], *ptr;
 	char s_time[MAX_TOKEN_SIZE], c_time[MAX_TOKEN_SIZE];
+	char line[MAX_WIDTH];
 	int i, j, tmp, tty_nr, fd;
+	int cur;
 
 	nice = atol(tokens[19]);
 	//high-priority
@@ -236,57 +304,120 @@ void print_uOption()
 	rss = atol(tokens[24]) * 4;
 	tmp = (int)(((double)rss / memtotal) * 1000);
 	//print
-	printf("%-9s %6s ",tokens[0], tokens[1]);
-	printf(" %.1f ", cpu_usage);
-	printf(" %.1f ", tmp/10.0);
-	printf("%6ld ",vsz);
-	printf("%5ld ",rss);
-	printf("%-8s ",tokens[7]);
-	printf("%-4s ",tokens[3]);
-	printf("%s ",s_time);
-	printf("%6s ",c_time);
-	printf("%s\n",path);
-
-
+	cur = 0;
+	memset(line, 0, sizeof(line));
+	sprintf(line,"%-7s %6s  %.1f  %.1f %6ld %5ld %s ",tokens[0], tokens[1], cpu_usage, tmp/10.0,vsz,rss, tokens[7]);
+	printf("%s",line);
+	//1차 print분기점
+	cur += strlen(line);
+	while(cur++ < 47)
+		printf(" ");
+	memset(line, 0, sizeof(line));
+	sprintf(line,"%-4s %s %6s ",tokens[3], s_time, c_time);
+	printf("%s",line);
+	cur += strlen(line);
+	//2차 print분기점
+	memset(line, 0, sizeof(line));
+	strncpy(line, path, MAX_WIDTH-cur+1);
+	printf("%s\n",line);
 }
-void get_memtotal()
-{
-	FILE* fp;
-	char token[MAX_TOKEN_SIZE];
-	int c, i;
-	if((fp = fopen("/proc/meminfo", "r")) == NULL){
-		fprintf(stderr,"meminfo open error\n");
+void print_one_option(){
+	long nice, num_thread;
+	unsigned long long starttime,totaltime;
+	char path[MAX_INPUT_SIZE], *ptr;
+	char c_time[MAX_TOKEN_SIZE], line[MAX_WIDTH];
+	int i, j, tty_nr, fd, cur;
+
+	nice = atol(tokens[19]);
+	//high-priority
+	if(nice < 0)
+		strcat(tokens[3], "<");
+	//low-priority
+	else if(nice > 0)
+		strcat(tokens[3], "N");
+	//session leader
+	if(!strcmp(tokens[1], tokens[6]))
+		strcat(tokens[3], "s");
+	num_thread = atol(tokens[20]);
+	//multithread
+	if(num_thread > 1)
+		strcat(tokens[3], "l");
+	//foreground
+	if(!strcmp(tokens[5], tokens[8]))
+		strcat(tokens[3], "+");
+	//is tty
+	if(tokens[7][0] == '1'){
+		tty_nr = atoi(tokens[7]);
+		memset(tokens[7], 0, sizeof(tokens[7]));
+		sprintf(tokens[7],"tty%d",tty_nr-1024);
+	}
+	//is pts
+	else if(tokens[7][0] == '3'){
+		tty_nr = atoi(tokens[7]);
+		memset(tokens[7], 0, sizeof(tokens[7]));
+		sprintf(tokens[7],"pts/%d",tty_nr-34816);
+	}
+	//else
+	else{
+		memset(tokens[7], 0, sizeof(tokens[7]));
+		strcpy(tokens[7], "?");
+	}
+	//COMMAND
+	if((fd = open("cmdline", O_RDONLY)) < 0){
+		fprintf(stderr,"cmdline open error\n");
 		exit(1);
 	}
-	while((c = fgetc(fp)) != '\n'){
-		if(c >= '0' && c <= '9'){
-			i = 0;
-			token[i++] = c;
-			c = fgetc(fp);
-			while(c >= '0' && c <= '9'){
-				token[i++] = c;
-				c = fgetc(fp);
-			}
-			token[i] = '\0';
-			break;
+	memset(path, 0, sizeof(path));
+	//임시로 cmdline이 1024자 이하라고 가정
+	j = read(fd, path, sizeof(path));
+	if(j != 0){
+		if(strlen(path) < j){
+			ptr = path;
+			while(ptr-path <= j &&(ptr = strchr(ptr, '\0')) != NULL)
+				*ptr++ = ' ';
+			path[j-1] = '\0';
 		}
-		
 	}
-	memtotal = atol(token);
+	else
+		sprintf(path,"[%s]",tokens[2]);
+	close(fd);
+	totaltime = 0;
+	//totaltime
+	for(i = 14; i < 16; i++){
+		totaltime += strtoul(tokens[i], NULL, 10);
+	}
+	//get time info
+	starttime = strtoull(tokens[22], NULL, 10);
+	get_info(totaltime, starttime);
+	if(cpu_time >= 60){
+		memset(c_time, 0, sizeof(c_time));
+		sprintf(c_time,"%ld:%02ld",cpu_time/60, cpu_time%60);
+	}
+	else{
+		memset(c_time, 0, sizeof(c_time));
+		sprintf(c_time,"0:%02ld",cpu_time);
+	}
+
+	//print
+	memset(line, 0, sizeof(line));
+	sprintf(line, "%5s %-8s %-6s %s ", tokens[1], tokens[7], tokens[3], c_time);
+	printf("%s",line);
+	cur = strlen(line);
+	memset(line, 0, sizeof(line));
+	strncpy(line, path, MAX_WIDTH - cur);
+	printf("%s\n",line);
+
 }
 void print_no_option()
 {
 	FILE* fp;
 	DIR* dirp;
 	struct dirent* dp;
-	char path[MAX_INPUT_SIZE];
+	char path[MAX_INPUT_SIZE], line[MAX_WIDTH];
 	char tty_num[MAX_TOKEN_SIZE],c_time[MAX_TOKEN_SIZE];
-	char *ptr;
 	pid_t pid;
-	int i, j, c, tty_nr;
-	long cmdsize;
+	int i, j, c, tty_nr, cur;
 	unsigned long long totaltime, starttime;
-	time_t t;
 
 	pid = getpid();
 	memset(path, 0, sizeof(path));
@@ -372,10 +503,14 @@ void print_no_option()
 			}
 			else
 				sprintf(c_time,"00:00:%02ld",cpu_time%60);
-			printf("%7s ",tokens[1]);
-			printf("%-8s ",tokens[7]);
-			printf("%s ",c_time);
-			printf("%s\n",tokens[2]);
+
+			memset(line, 0, sizeof(line));
+			sprintf(line, "%5s %-8s %s ",tokens[1], tokens[7], c_time);
+			printf("%s",line);
+			cur = strlen(line);
+			memset(line, 0, sizeof(line));
+			strncpy(line, tokens[2], MAX_WIDTH-cur);
+			printf("%s\n",line);
 			fclose(fp);
 			chdir("..");
 		}
